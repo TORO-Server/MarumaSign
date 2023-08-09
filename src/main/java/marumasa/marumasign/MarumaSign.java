e64a8d61aa7320ca9563916e5ed5f99443de9e02
malken21 <Malken_Marumasa@outlook.jp>
2023-08-09 11:48:11 +0900

    Setup

---
+++ src/main/java/marumasa/marumasign/MarumaSign.java
@@ -0,0 +1,10 @@
+package marumasa.marumasign;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MarumaSign implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/marumasa/marumasign/client/MarumaSignClient.java
@@ -0,0 +1,10 @@
+package marumasa.marumasign.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MarumaSignClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
