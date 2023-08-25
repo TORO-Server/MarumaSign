293294fbe276508f0f87817cf5f6259aec35db8c
malken21 <Malken_Marumasa@outlook.jp>
2023-08-26 03:16:17 +0900

    Update TextureURL.java

--- src/main/java/marumasa/marumasa_sign/type/TextureURL.java
+++ src/main/java/marumasa/marumasa_sign/type/TextureURL.java
@@ -5,7 +5,7 @@ import net.minecraft.util.Identifier;
 
 public record TextureURL(Identifier identifier, int width, int height) {
     public static final TextureURL loading = new TextureURL(
-            new Identifier("textures/gui/container/anvil.png"), 1, 1
+            new Identifier(MarumaSign.MOD_ID, "textures/misc/loading.png"), 1, 1
     );
     public static final TextureURL error = new TextureURL(
             new Identifier(MarumaSign.MOD_ID, "textures/misc/error.png"), 960, 576
