109b5c977ae87b2e3887a053f82caf23643cff88
malken21 <Malken_Marumasa@outlook.jp>
2023-10-18 20:43:55 +0900

    Update GifFrame.java

--- src/main/java/marumasa/marumasa_sign/type/GifFrame.java
+++ src/main/java/marumasa/marumasa_sign/type/GifFrame.java
@@ -1,5 +1,6 @@
 package marumasa.marumasa_sign.type;
 
+import marumasa.marumasa_sign.MarumaSign;
 import net.minecraft.client.render.RenderLayer;
 
 import java.util.NavigableMap;
@@ -7,11 +8,15 @@ import java.util.NavigableMap;
 public class GifFrame {
     public final String stringURL;
     public final NavigableMap<Integer, RenderLayer> frameMap;
+    public final int repetitions;
 
     public int frame = 0;
+    public int repeat_count = 0;
 
-    public GifFrame(String stringURL, NavigableMap<Integer, RenderLayer> frameMap) {
+    public GifFrame(String stringURL, NavigableMap<Integer, RenderLayer> frameMap, int repetitions) {
         this.stringURL = stringURL;
         this.frameMap = frameMap;
+        this.repetitions = repetitions;
+        MarumaSign.LOGGER.info(String.valueOf(repetitions));
     }
 }
