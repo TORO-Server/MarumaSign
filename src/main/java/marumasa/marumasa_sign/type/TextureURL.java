26528e3cc93cca03ca0a0652bf2215e5dde530f6
malken21 <77374813+malken21@users.noreply.github.com>
2024-03-22 03:57:53 +0900

    リソースの画像ファイルの大きさ設定を変更

--- src/main/java/marumasa/marumasa_sign/type/TextureURL.java
+++ src/main/java/marumasa/marumasa_sign/type/TextureURL.java
@@ -4,10 +4,14 @@ import marumasa.marumasa_sign.MarumaSign;
 import net.minecraft.util.Identifier;
 
 public record TextureURL(Identifier identifier, int width, int height) {
+
+    // 読み込み中のテクスチャ
     public static final TextureURL loading = new TextureURL(
-            new Identifier(MarumaSign.MOD_ID, "textures/misc/loading.png"), 1, 1
+            new Identifier(MarumaSign.MOD_ID, "textures/misc/loading.png"), 160, 96
     );
+
+    // 読み込み失敗のテクスチャ
     public static final TextureURL error = new TextureURL(
-            new Identifier(MarumaSign.MOD_ID, "textures/misc/error.png"), 960, 576
+            new Identifier(MarumaSign.MOD_ID, "textures/misc/error.png"), 160, 96
     );
 }
